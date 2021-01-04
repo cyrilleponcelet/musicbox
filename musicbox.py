48d0c593a89ac0ce5460326ca535cc857b677ad4
Cyrille Poncelet <cyrilleponcelet@users.noreply.github.com>
2021-01-04 07:32:11 +0100

    First release of musicbox

--- musicbox.py
+++ musicbox.py
@@ -3,93 +3,97 @@ from subprocess import check_call
 import vlc
 import glob
 import re
+import time
 
-# static stuff
-media_player = None
-input_dir = '/home/pi/Music/LeSoldatRose'
+class Player:
 
-
-def main_loop():
-    from signal import pause
-    pause()
-    pass
+    def __init__(self, input_dir='/home/pi/Music/LeSoldatRose'):
+        # static stuff
+        self.media_player = None
+        self.input_dir = input_dir
 
 
-def shutdown():
-    play_single_file("./termine.mp3")
-    check_call(['sudo', 'poweroff'])
+    def main_loop(self):
+        from signal import pause
+        pause()
+        pass
+
+    def shutdown(self):
+        print("Shutdown")
+        self.play_single_file("./termine.mp3")
+        time.sleep(1)
+        print("Really Shutdown")
+        check_call(['sudo', 'poweroff'])
 
 
-def _next():
-    media_player.next()
+    def _next(self):
+        self.media_player.next()
 
-def _pause():
-    media_player.pause()
+    def _pause(self):
+        self.media_player.pause()
 
-def _backward():
-    media_player.previous()
-
-
-
-def parse_directory(input_dir: str):
-    """ Parse a directory and return file content"""
-    print(f"Parsing {input_dir}")
-    files = glob.glob(f"{input_dir}/*mp3")
-    file_list = sorted(files, key=lambda x: float(re.findall("(\d+)", x)[0]))
-    return file_list
+    def _backward(self):
+        self.media_player.previous()
 
 
-def play_single_file(input_file:str):
-    player = vlc.Instance()
-    media_list = player.media_list_new()
+
+    def parse_directory(self,input_dir: str):
+        """ Parse a directory and return file content"""
+        print(f"Parsing {input_dir}")
+        files = glob.glob(f"{input_dir}/*mp3")
+        file_list = sorted(files, key=lambda x: float(re.findall("(\d+)", x)[0]))
+        return file_list
 
 
-    pn = player.media_new(input_file)
-
-
-    media_list.add_media(pn)
-    media_player.set_media_list(media_list)
-
-
-def create_playlist(input_dir: str):
-    """ parse a directory and create playlist"""
-    file_list = parse_directory(input_dir)
-    player = vlc.Instance()
-    media_list = player.media_list_new()
-    for f in file_list:
-        print(f"Adding {f} to playlist")
-        pn = player.media_new(f)
+    def play_single_file(self,input_file:str):
+        player = vlc.Instance()
+        media_list = player.media_list_new()
+        pn = player.media_new(input_file)
         media_list.add_media(pn)
-    media_player.set_media_list(media_list)
-    media_player.play_item_at_index(0)
+        self.media_player.set_media_list(media_list)
+        self.media_player.play_item_at_index(0)
+
+
+    def create_playlist(self,input_dir: str):
+        """ parse a directory and create playlist"""
+        file_list = self.parse_directory(input_dir)
+        player = vlc.Instance()
+        media_list = player.media_list_new()
+        for f in file_list:
+            print(f"Adding {f} to playlist")
+            pn = player.media_new(f)
+            media_list.add_media(pn)
+        self.media_player.set_media_list(media_list)
+        self.media_player.play_item_at_index(0)
 
 
 
 
-def main():
-    # player = vlc.MediaPlayer('termine.mp3')
-    print("create media player")
-    media_player = vlc.MediaListPlayer()
-    print("create playlist")
-    create_playlist(input_dir)
-    button_gauche = Button(4)
-    button_droit = Button(3)
-    button_milieu = Button(2, hold_time=3)
+    def run(self):
+        # player = vlc.MediaPlayer('termine.mp3')
+        print("create media player")
+        self.media_player = vlc.MediaListPlayer()
+        print("create playlist")
+        self.create_playlist(self.input_dir)
+        button_gauche = Button(4)
+        button_droit = Button(3)
+        button_milieu = Button(2, hold_time=3)
 
 
-    button_gauche.when_pressed = _backward
-    button_droit.when_pressed = _next
+        button_gauche.when_pressed = self._backward
+        button_droit.when_pressed = self._next
 
-    button_milieu.when_pressed = _pause
-    button_milieu.when_held = shutdown
+        button_milieu.when_pressed =self._pause
+        button_milieu.when_held = self.shutdown
 
+        #read first song
+        self.media_player.play_item_at_index(0)
 
-    media_player.play_item_at_index(0)
-
-
-    main_loop()
+        #pause and wait
+        self.main_loop()
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main()
+    player = Player()
+    player.run()
