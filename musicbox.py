de2d4414972964856cd3eb919da49b0f2f91b180
cponcele <cyrille.poncele@ifremer.fr>
2021-02-14 20:41:29 +0100

    update musicbox to add directories

--- musicbox.py
+++ musicbox.py
@@ -6,23 +6,21 @@ import re
 import time
 
 
-
 class Player:
-
-    # match directories and their associated files to play
-    directories= {"Odyssee": ('/home/pi/Music/Odyssee',"./odyssee.m4a"),
-                      "Olma": ('/home/pi/Music/Olma',"./olma.m4a"),
-                      "LeSoldatRose":('/home/pi/Music/LeSoldatRose',"./SoldatRose.m4a")
-                      }
-
-    def __init__(self, first_dir="Odyssee"):
+    def __init__(self, first_dir="odyssee"):
         # static stuff
         self.media_player = None
         self.current_dir = first_dir
-
+        # match directories and their associated files to play
+        self.directory_names = {
+            "odyssee": ("/home/pi/Music/Odyssee", "./odyssee.mp3"),
+            "olma": ("/home/pi/Music/Olma", "./olma.mp3"),
+            "lesoldatrose": ("/home/pi/Music/LeSoldatRose", "./SoldatRose.mp3"),
+        }
 
     def main_loop(self):
         from signal import pause
+
         pause()
         pass
 
@@ -31,12 +29,12 @@ class Player:
         self.play_single_file("./termine.mp3")
         time.sleep(1)
         print("Really Shutdown")
-        check_call(['sudo', 'poweroff'])
+        check_call(["sudo", "poweroff"])
 
     def _next_dir(self):
-        keys = self.directories.keys
+        keys = list(self.directory_names.keys())
         current_idx = keys.index(self.current_dir)
-        current_idx = current_idx+1
+        current_idx = current_idx + 1
         if current_idx >= len(keys):
             current_idx = 0
         self.current_dir = keys[current_idx]
@@ -52,9 +50,7 @@ class Player:
     def _backward(self):
         self.media_player.previous()
 
-
-
-    def parse_directory(self,dir_name: str):
+    def parse_directory(self, dir_name: str):
         """ Parse a directory and return file content"""
         input_dir = self.directory_names[dir_name.lower()][0]
         print(f"Parsing {input_dir}")
@@ -62,10 +58,10 @@ class Player:
 
         files = glob.glob(f"{input_dir}/*mp3")
         file_list = sorted(files, key=lambda x: float(re.findall("(\d+)", x)[0]))
+        time.sleep(2)
         return file_list
 
-
-    def play_single_file(self,input_file:str):
+    def play_single_file(self, input_file: str):
         player = vlc.Instance()
         media_list = player.media_list_new()
         pn = player.media_new(input_file)
@@ -73,8 +69,7 @@ class Player:
         self.media_player.set_media_list(media_list)
         self.media_player.play_item_at_index(0)
 
-
-    def create_playlist(self,dir_name: str):
+    def create_playlist(self, dir_name: str):
         """ parse a directory and create playlist"""
         file_list = self.parse_directory(dir_name)
         player = vlc.Instance()
@@ -86,9 +81,6 @@ class Player:
         self.media_player.set_media_list(media_list)
         self.media_player.play_item_at_index(0)
 
-
-
-
     def run(self):
         # player = vlc.MediaPlayer('termine.mp3')
         print("create media player")
@@ -97,20 +89,20 @@ class Player:
         self.create_playlist(self.current_dir)
         button_gauche = Button(4)
         button_droit = Button(3, hold_time=1)
-        button_milieu = Button(2, hold_time=1)
-
+        button_milieu = Button(2, hold_time=2)
 
         button_gauche.when_pressed = self._backward
+
         button_droit.when_pressed = self._next
         button_droit.when_held = self._next_dir
 
-        button_milieu.when_pressed =self._pause
+        button_milieu.when_pressed = self._pause
         button_milieu.when_held = self.shutdown
 
-        #read first song
+        # read first song
         self.media_player.play_item_at_index(0)
 
-        #pause and wait
+        # pause and wait
         self.main_loop()
 
 
